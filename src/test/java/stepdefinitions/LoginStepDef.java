0be93d93f1484a2516611a4a257b17651a851680
Neeraj singh <nirmalapanghal43@gmail.com>
2024-04-14 18:49:26 +0530

    Scenoios and step def

--- src/test/java/stepdefinitions/LoginStepDef.java
+++ src/test/java/stepdefinitions/LoginStepDef.java
@@ -1,4 +1,35 @@
 package stepdefinitions;
 
-public class LoginStepDef {
+public class LoginStepDef
+{
+    @Given("i open the browser with url")
+    public void i_open_the_browser_with_url() {
+
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("i should the login screen")
+    public void i_should_the_login_screen() {
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("i enter the username  as {string}")
+    public void i_enter_the_username_as(String string) {
+
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("i enter password as {string}")
+    public void i_enter_password_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("i click login")
+    public void i_click_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("i should see admin Dashboard")
+    public void i_should_see_admin_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
