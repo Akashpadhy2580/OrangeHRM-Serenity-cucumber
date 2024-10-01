7d0f069b054cae92164353aeef7493988c3f50a2
Akash <akashpadhy2580@gmail.com>
2024-10-02 00:57:30 +0530

    Initial Commit

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,10 +3,9 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/featureFiles",
+@CucumberOptions(features = "src/test/resources/features",
                 glue = "stepdefinitions",
                 plugin = {"pretty","html:target/Reports/Cucumber-Report.html"},
                 tags = "@orangehrm")
-
 public class TestRunner {
 }
